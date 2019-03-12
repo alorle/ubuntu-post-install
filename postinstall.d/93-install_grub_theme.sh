1ca17ffe3715723d68822f3046667c05d0ab32be
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2019-03-12 17:15:49 +0100

    theming: Simplify theming scripts
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- postinstall.d/93-install_grub_theme.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget -O - https://github.com/shvchk/poly-light/raw/master/install.sh | $sh_c bash
--- postinstall.d/94-configure_desktop.sh
+++ postinstall.d/94-configure_desktop.sh
@@ -3,4 +3,4 @@
 $sh_c "gsettings set org.gnome.desktop.interface gtk-theme 'Communitheme-dark'"
 $sh_c "gsettings set org.gnome.desktop.interface icon-theme 'communitheme'"
 $sh_c "gsettings set org.gnome.desktop.interface cursor-theme 'whiteglass'"
-$sh_c "gsettings set org.gnome.desktop.wm.preferences theme 'Adwaita'"
+# $sh_c "gsettings set org.gnome.desktop.wm.preferences theme 'Adwaita'"
